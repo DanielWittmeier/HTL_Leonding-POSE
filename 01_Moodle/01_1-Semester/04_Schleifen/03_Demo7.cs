3863228559371f2ed65162cb121fc806f1c97b99
Dani Wittmeier <d.wittmeier@students.htl-leonding.ac.at>
2025-03-19 14:12:47 +0100

    Ordnerstruktur angelegt + Theorie

---
+++ 01_Moodle/01_1-Semester/04_Schleifen/03_Demo7.cs
@@ -0,0 +1,24 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: xAHIF
+ *--------------------------------------------------------------
+ * Description: Summe
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+string output = "";
+
+Console.Write("Bitte geben sie einen Wert ein: ");
+
+int val = int.Parse(Console.ReadLine());
+
+while (val > 0)
+{
+    int digit = val % 2;
+    val = val / 2;
+
+    output = $"{digit}{output}";
+}
+
+Console.WriteLine(output);
---
+++ 01_Moodle/01_1-Semester/05_Strings/02_FindStringInString.cs
@@ -0,0 +1,34 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: xAHIF
+ *--------------------------------------------------------------
+ * Description: FindInString
+ *--------------------------------------------------------------
+ */
+
+using System;
+
+string text = "Hallo Welt!ll";
+string search = "";
+
+//int idx = text.IndexOf(search);
+
+int idx = -1;
+
+for (int i = 0; i < text.Length && idx == -1; i++)
+{
+    int n;
+    for (n = 0; 
+         n < search.Length && 
+           (n+i) < text.Length &&
+           text[i + n] == search[n];
+         n++)
+    {
+    }
+
+    if (n == search.Length)
+    {
+        idx = i;
+    }
+}
+
+Console.WriteLine($"\"{text}\" in \"{search}\" at index {idx}");
