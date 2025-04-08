a0745538afb9a4c99d220b52341494aec9320d5e
Dani Wittmeier <d.wittmeier@students.htl-leonding.ac.at>
2025-04-08 19:13:06 +0200

    Übung 22_01_DameProblem erledigt.

---
+++ 01_Moodle/02_2-Semester/02_Zweidimensionale-Arrays/PE_22_08-04-2025/22_01_DameProblem/Code/DameProblem/Dame.cs
@@ -0,0 +1,68 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 4ABIFT
+*--------------------------------------------------------------
+*              Dani Wittmeier
+*--------------------------------------------------------------
+* Description: Solve DameProblem
+* see: https://de.wikipedia.org/wiki/Damenproblem
+*--------------------------------------------------------------
+*/
+
+namespace DameProblem
+{
+    public class Dame
+    {
+        public static bool IsValid(bool[,] field)
+        {
+            
+            if (field[0,0] && field[1,1] || field[field.GetLength(0)-1,0] && field[field.GetLength(1)-2, 1])
+            {
+                return false;
+            }
+            
+            int numberges = 0;
+
+            for ( int x = 0; x < field.GetLength(0); x++ )
+            {
+                int number = 0;
+                for ( int y = 0; y < field.GetLength(1); y++ )
+                {
+                    if (field[x,y])
+                    {
+                        numberges++;
+                        number++;
+                        if (number > 1 )
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < field.GetLength(1); y++)
+            {
+                int number = 0;
+                for (int x = 0; x < field.GetLength(0); x++)
+                {
+                    if (field[x, y])
+                    {
+                        number++;
+                        if (number > 1)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            if (numberges == 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 01_Moodle/02_2-Semester/02_Zweidimensionale-Arrays/PE_22_08-04-2025/22_01_DameProblem/Code/DameProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿/*--------------------------------------------------------------
+*				HTBLA-Leonding / Class: 4ABIFT
+*--------------------------------------------------------------
+*              Dani Wittmeier
+*--------------------------------------------------------------
+* Description: DameProblem with UnitTests
+*--------------------------------------------------------------
+*/
+
+using System;
+
+namespace DameProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
