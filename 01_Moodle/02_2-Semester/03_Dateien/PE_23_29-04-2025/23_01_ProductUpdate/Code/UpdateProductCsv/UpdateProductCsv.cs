000b04da4627c030e3d761c8d9c40fbbb9b29022
Dani Wittmeier <d.wittmeier@students.htl-leonding.ac.at>
2025-04-29 22:00:12 +0200

    Erster Versuch von Übung 23_01
    Ersten Versuch erledigt, Finalisierung kommt im laufe der Woche.

--- 01_Moodle/02_2-Semester/03_Dateien/PE_23_29-04-2025/23_01_ProductUpdate/Code/UpdateProductCsv/UpdateProductCsv.cs
+++ 01_Moodle/02_2-Semester/03_Dateien/PE_23_29-04-2025/23_01_ProductUpdate/Code/UpdateProductCsv/UpdateProductCsv.cs
@@ -10,6 +10,7 @@
 namespace UpdateProductCsv
 {
     using System;
+    using System.Formats.Asn1;
     using System.Globalization;
     using System.IO;
     using System.IO.Enumeration;
@@ -20,21 +21,30 @@ namespace UpdateProductCsv
         {
             //TODO: Implement Main method Check / Lesen / Aktualisieren / Speichern
             string filename;
-            double percent;
+            decimal percent;
             int fehlercode;
 
             if (!CheckArguments(args, out filename, out percent, out fehlercode))
             {
-                return fehlercode;
+                return 1;
             }
 
-            return fehlercode; //0 heißt kein Fehler, 1 für Fehler
+            string[] csvfile = ReadCSVFiles(filename);
+
+            //File.Create(Path.GetFileNameWithoutExtension(args[0]) + ".$$$");
+            File.Delete(Path.GetFileNameWithoutExtension(args[0]) + ".bak");
+            File.Move(filename, Path.GetFileNameWithoutExtension(args[0]) + ".bak");
+
+            File.WriteAllLines(Path.GetFileNameWithoutExtension(args[0]) + ".$$$", UpdateCSVFile(csvfile, percent));
+            File.Move(Path.GetFileNameWithoutExtension(args[0]) + ".$$$", Path.GetFileNameWithoutExtension(args[0]) + ".csv");
+
+            return 0; //0 heißt kein Fehler, 1 für Fehler
         }
 
         // TODO: Implement methods for Read, Update and Write Csv 
-        private static bool CheckArguments(string[] args, out string filename, out double percent, out int fehlercode)
+        private static bool CheckArguments(string[] args, out string filename, out decimal percent, out int fehlercode)
         {
-            filename = args[0];
+            filename = "";
             percent = 0;
             fehlercode = 0;
 
@@ -43,19 +53,47 @@ namespace UpdateProductCsv
                 fehlercode = 1;
                 return false;
             }
-
+            filename = args[0];
             if (!File.Exists(filename))
             {
                 fehlercode = 2;
                 return false;
             }
 
-            if (!double.TryParse(args[1], CultureInfo.InvariantCulture , out percent))
+            if (Path.GetExtension(filename).ToUpper() != ".CSV")
             {
                 fehlercode = 3;
                 return false;
             }
+
+            if (!decimal.TryParse(args[1], CultureInfo.InvariantCulture , out percent))
+            {
+                fehlercode = 4;
+                return false;
+            }
+
+            percent = 1 + percent / 100;
             return true;
+        }
+
+        private static string[] ReadCSVFiles(string filename)
+        {
+            return File.ReadAllLines(filename);
+        }
+
+        private static string[] UpdateCSVFile(string[] csvfile, decimal percent)
+        {
+            string[] newcsvfile = new string[csvfile.Length];
+
+            for (int i = 1; i < csvfile.Length; i++) 
+            {
+                Product.ConvertOneLine(csvfile[i], percent, out string productCode, out string description, out string taxClass, out decimal retail);
+                newcsvfile[i] = productCode + ";" + description + ";" + taxClass + ";" + retail;
+
+            }
+
+
+            return newcsvfile;
         }
     }
 }
